1a0eabe81e41c5d1fa0440a7f5530c739e2eccc0
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 07:56:57 +0530

    Coding done

--- src/main/java/com/jap/GameDemo.java
+++ src/main/java/com/jap/GameDemo.java
@@ -1,18 +1,35 @@
 package com.jap;
 
-public class GameDemo {
 
-    public String divideByNumber(int firstNumber, int secondNumber){
+import java.util.Scanner;
+
+public class GameDemo
+{
+
+    public String divideByNumber(int firstNumber, int secondNumber)
+    {
         //create local variable
         int finalNumber = 0;
         try {
             //write logic
-
+                 finalNumber = firstNumber / secondNumber;
             //Handle specific exception
-        }catch (Exception exception){
+        }
+        catch (ArithmeticException exception)
+        {
+
           return exception.toString();
         }
         return String.valueOf(finalNumber);
+    }
+
+    public static void main(String[] args) {
+        GameDemo gameDemo = new GameDemo();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two integer numbers :");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        System.out.println(gameDemo.divideByNumber(firstNumber, secondNumber));
     }
 
 
